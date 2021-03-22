8c3f92a773591d85f74e6d8b96436257b595ba09
Dara Olayebi <dara.olayebi@gmail.com>
2021-03-22 21:02:52 +0100

    first commit

---
+++ components/AppHeader.vue
@@ -0,0 +1,35 @@
+<template>
+    <header>
+        <h1 class="title">Project SafeUp</h1>
+        <nav>
+            <ul>
+                <li>
+                    <nuxt-link to="/">Home</nuxt-link>
+                </li>
+                <li>
+                    <nuxt-link to="/impact">Impact</nuxt-link>
+                </li>
+                <li>
+                    <nuxt-link to="/gallery">Gallery</nuxt-link>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+	name: "AppHeader",
+};
+</script>
+
+<style>
+header {
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+}
+.title {
+	text-transform: uppercase;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+      <AppHeader/>
+    <Nuxt />
+  </div>
+</template>
+
+
+
+<style>
+body {
+	background-color: #fbf6f0;
+}
+</style>
